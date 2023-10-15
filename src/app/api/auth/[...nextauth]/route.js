a3c7d7f8663b0de2c965b2810982e2eff89c9a8b
Pranjal-622003 <pranjalsharma622003@gmail.com>
2023-10-15 22:38:44 +0530

    very good

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
+import { connectMongoDB } from "../../../../../lib/mongodb";
+import User from "../../../../../models/user";
+
 
 const authOptions={
     providers:[
@@ -9,6 +12,38 @@ const authOptions={
 
         }),
     ],
+    callbacks: {
+        async signIn({ user, account }) {
+          if (account.provider === "google") {
+            const { name, email } = user;
+            try {
+              await connectMongoDB();
+              const userExists = await User.findOne({ email });
+    
+              if (!userExists) {
+                const res = await fetch("http://localhost:3000/api/user", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    name,
+                    email,
+                  }),
+                });
+    
+                if (res.ok) {
+                  return user;
+                }
+              }
+            } catch (error) {
+              console.log(error);
+            }
+          }
+    
+          return user;
+        },
+      },
 };
 
 const handler=NextAuth(authOptions)
